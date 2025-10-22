4ffe0fce47fd441e4fe01feb3faa258df1103678
suhlaingoodev107 <dev107.ovmyanmar@gmail.com>
2025-10-22 12:23:32 +0630

    Lecture 14 - Adding TextFormField Theme

--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -65,5 +65,44 @@ ThemeData getApplicationTheme() {
     ),
 
     // input decoration theme (text form field)
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(AppPadding.p8),
+      // hint style
+      hintStyle: getRegularStyle(color: ColorManager.grey1),
+
+      // label style
+      labelStyle: getMediumStyle(color: ColorManager.darkGrey),
+      // error style
+      errorStyle: getRegularStyle(color: ColorManager.error),
+
+      // enabled border
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+
+      // focused border
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+
+      // error border
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.error, width: AppSize.s1_5),
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+      // focused error border
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+    ),
   );
 }
