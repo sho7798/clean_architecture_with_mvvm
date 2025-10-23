07185c4beb05e7c67b21b69d6fbca5f65034fc5f
suhlaingoodev107 <dev107.ovmyanmar@gmail.com>
2025-10-23 15:38:19 +0630

    Lecture 47 - Data Layer : Add API service client retrofit

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,15 @@
+import 'package:clean_architecture_mvvm/data/responses/responses.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+import 'package:clean_architecture_mvvm/app/constant.dart';
+
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constant.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login();
+}
