122d18d89ca5927c12179d578d177414b27dffce
suhlaingoodev107 <dev107.ovmyanmar@gmail.com>
2025-10-23 16:05:04 +0630

    Lecture 50 - Data Layer: Creating Mapper

---
+++ lib/data/mapper/mapper.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:clean_architecture_mvvm/app/extension.dart';
+import 'package:clean_architecture_mvvm/data/responses/responses.dart';
+import 'package:clean_architecture_mvvm/domain/model/model.dart';
+
+const EMPTY = "";
+const ZERO = 0;
+
+extension CustomerResponseMapper on CustomerResponse? {
+  Customer toDomain() {
+    return Customer(
+      id: this?.id?.orEmpty() ?? EMPTY,
+      name: this?.name?.orEmpty() ?? EMPTY,
+      numOfNotifications: this?.numOfNotifications?.orZero() ?? ZERO,
+    );
+  }
+}
+
+extension ContactsResponseMapper on ContactsResponse? {
+  Contacts toDomain() {
+    return Contacts(
+      email: this?.email?.orEmpty() ?? EMPTY,
+      phone: this?.phone?.orEmpty() ?? EMPTY,
+      link: this?.link?.orEmpty() ?? EMPTY,
+    );
+  }
+}
+
+extension AuthenticationResponseMapper on AuthenticationResponse? {
+  Authentication toDomain() {
+    return Authentication(
+      customer: this?.customer?.toDomain(),
+      contacts: this?.contacts?.toDomain(),
+    );
+  }
+}
