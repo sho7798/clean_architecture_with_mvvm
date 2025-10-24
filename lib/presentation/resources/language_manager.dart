1bb9da34f34c72dd7efafe6b87073333ff3c2658
suhlaingoodev107 <dev107.ovmyanmar@gmail.com>
2025-10-24 13:41:27 +0630

    Lecture 64 - Presentation Layer: Adding Language Manager

---
+++ lib/presentation/resources/language_manager.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: constant_identifier_names
+
+enum LanguageType { ENGLISH, MYANMAR }
+
+const String MYANMAR = "mm";
+const String ENGLISH = "en";
+
+extension LanguageTypeExtension on LanguageType {
+  String getValue() {
+    switch (this) {
+      case LanguageType.ENGLISH:
+        return ENGLISH;
+      case LanguageType.MYANMAR:
+        return MYANMAR;
+    }
+  }
+}
