a2ce5a12c665f82e9b8663cdb2a8e549b936d207
suhlaingoodev107 <dev107.ovmyanmar@gmail.com>
2025-10-24 12:47:18 +0630

    Lecture 62 - Data Layer: Adding Dio Factory

---
+++ lib/data/network/dio_factory.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:clean_architecture_mvvm/app/constant.dart';
+import 'package:dio/dio.dart';
+
+const String APPLICATION_JSON = "application/json";
+const String CONTENT_TYPE = "content/type";
+const String ACCEPT = "accept";
+const String AUTHORIZATION = "authorization";
+const String DEFAULT_LANGUAGE = "language";
+
+class DioFactory {
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+    int timeout = 60 * 1000; //1 min
+    Map<String, dynamic> headers = {
+      CONTENT_TYPE: APPLICATION_JSON,
+      ACCEPT: APPLICATION_JSON,
+      AUTHORIZATION: Constant.token,
+      DEFAULT_LANGUAGE: "en",
+    };
+
+    dio.options = BaseOptions(
+      baseUrl: Constant.baseUrl,
+      connectTimeout: Duration(minutes: timeout),
+      receiveTimeout: Duration(minutes: timeout),
+      headers: headers,
+    );
+
+    return dio;
+  }
+}
