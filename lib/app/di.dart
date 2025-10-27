36e8a796779b2ebd034dc33155bc3b910a8fe0c7
suhlaingoodev107 <dev107.ovmyanmar@gmail.com>
2025-10-27 10:37:43 +0630

    Lecture 85 - Add Dependency Login Module

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -4,6 +4,8 @@ import 'package:clean_architecture_mvvm/data/network/dio_factory.dart';
 import 'package:clean_architecture_mvvm/data/network/network_info.dart';
 import 'package:clean_architecture_mvvm/data/repository/repository_impl.dart';
 import 'package:clean_architecture_mvvm/domain/repository/repository.dart';
+import 'package:clean_architecture_mvvm/domain/usecase/login_usecase.dart';
+import 'package:clean_architecture_mvvm/presentation/login/login_viewmodel.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
@@ -42,4 +44,11 @@ Future<void> initAppModule() async {
 
   // repository
   instance.registerLazySingleton<Repository>(() => RepositoryImpl());
+}
+
+initLoginModule() {
+  if (!GetIt.I.isRegistered<LoginUsecase>()) {
+    instance.registerFactory<LoginUsecase>(() => LoginUsecase());
+    instance.registerFactory<LoginViewModel>(() => LoginViewModel(instance()));
+  }
 }
